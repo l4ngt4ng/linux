66fd15d6dc11db8c8ad11acb61771490d6e97ded
albertouc3m <albertouc3m@gmail.com>
2024-01-16 06:50:01 +0100

    initial upload

---
+++ scripts/decompressor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#decomprime recursivamente un archivo comprimido que tine a su vez n archvos comprimidos recusivamente dentro hasta que pete porque 
+#el ultimo no sea un arcvhivo comprimido. 
+#usa 7z que con l te dice que contiene y con x decomprime sea lo que sea un tar un zip un gzip ...
+
+function ctrl_c(){
+  echo -e "\n\n[!] Saliendo...\n"
+  exit 1
+}
+
+# Ctrl+C
+trap ctrl_c INT
+
+first_file_name="data.gz"
+decompressed_file_name="$(7z l data.gz | tail -n 3 | head -n 1 | awk 'NF{print $NF}')"
+
+7z x $first_file_name &>/dev/null
+
+while [ $decompressed_file_name ]; do
+  echo -e "\n[+] Nuevo archivo descomprimido: $decompressed_file_name"
+  7z x $decompressed_file_name &>/dev/null
+  decompressed_file_name="$(7z l $decompressed_file_name 2>/dev/null | tail -n 3 | head -n 1 | awk 'NF{print $NF}')"
+done
